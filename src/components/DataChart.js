56a236ffeb88920fdad8216fbd9decc9213cbda6
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-10 22:25:12 +0600

    correct the previus commit

--- src/components/DataChart.js
+++ src/components/DataChart.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import SideLinks from './SideLinks';
 
 const DataChart = () => {
     return (
         <div style={{display: 'grid', gridTemplateColumns: '20% 80%'}} className='m-5'>
             <div className='flex flex-col'>
-                <h1>Hello link</h1>
+                <SideLinks to='/population'>Population</SideLinks>
+                <SideLinks to='/area'>Area</SideLinks>
             </div>
-            <div className='ml-10 p-8 h-screen' style={{position: 'sticky', top:0, }}>
+            <div className='ml-10 bg-gray-100 p-8 h-screen' style={{position: 'sticky', top:0, }}>
                 gello chart
             </div>
         </div>
